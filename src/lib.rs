da3d377759ee374baf2e07be12b5ab3fe8cd56bb
Hemal Harindaka <harindaka@gmail.com>
2017-11-07 16:36:01 +0530

    added serde_json dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde_json::Value;
+use serde_json::from_str;
+
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
@@ -11,6 +17,9 @@ pub fn merge_configs(config_overrides: &Vec<ConfigSource>){
     for config in config_overrides{
         match config {
             &ConfigSource::StringContent(ref content) => {
+                //let mut config: Value = from_str(include_str!("config/config.json")).unwrap();
+                //merge(&mut config, &config_qa);
+                //merge(&mut config, config_qa);
                 println!("{}", content);
             },
             &ConfigSource::FileContent(ref path) => {
@@ -21,6 +30,31 @@ pub fn merge_configs(config_overrides: &Vec<ConfigSource>){
             }
         }
     }    
+}
+
+// fn merge(a: &mut Value, b: &Value) {
+//     match (a, b) {
+//         (&mut Value::Object(ref mut a), &Value::Object(ref b)) => {
+//             for (k, v) in b {
+//                 merge(a.entry(k.clone()).or_insert(Value::Null), v);
+//             }
+//         }
+//         (a, b) => {
+//             *a = b.clone();
+//         }
+//     }
+// }
+
+fn merge(a: &mut Value, b: Value) {
+    match (a, b) {
+        (a @ &mut Value::Object(_), Value::Object(b)) => {
+            let a = a.as_object_mut().unwrap();
+            for (k, v) in b {
+                merge(a.entry(k).or_insert(Value::Null), v);
+            }
+        }
+        (a, b) => *a = b,
+    }
 }
 
 
