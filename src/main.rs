8286604465a54983addf14ce06d25af4b42e63bc
Hemal Harindaka <harindaka@gmail.com>
2017-11-09 17:16:21 +0530

    renamed ConfigSource to ConfigurationSource

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ fn main(){
     
     let mut builder = ConfigurationBuilder::new(&ConfigurationSource::StringContent(String::from(r#"{"test0": "val0"}"#)));
 
-    builder.merge_source(&ConfigurationSource::StringContent(String::from(r#"{"test1": "val1"}"#)));
+    builder.merge_source(&ConfigurationSource::StringContent(String::from(r#"{"test1": 1}"#)));
 
     let config_sources = vec![
-        ConfigurationSource::StringContent(String::from(r#"{"test2": "val2"}"#)),
-        ConfigurationSource::StringContent(String::from(r#"{"test3": "val3"}"#))
+        ConfigurationSource::StringContent(String::from(r#"{"test2": 1.234, "nested": { "nested1": "nestedValue1" }}"#)),
+        ConfigurationSource::StringContent(String::from(r#"{"test3": true}"#))
         ];
 
-    builder.merge_sources(&config_sources);    
+    builder.merge_sources(&config_sources);  
 
-    println!("{}", builder.to_string_pretty());
+    //println!("{}", builder.to_string_pretty());
+    println!("{}", builder.to_enum().to_string());
 }
+
+//https://stackoverflow.com/questions/32956050/how-to-create-a-static-string-at-compile-time
