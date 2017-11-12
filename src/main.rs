4ee20cac11f744c603e3a5711dc48cb1667ea447
harindaka <harindaka@gmail.com>
2017-11-13 00:04:02 +0530

    renamed method to_out_file to to_compiled

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main(){
 }
 
 // fn buildrs(){
-//     let builder = ConfigurationBuilder::new(ConfigurationSource::StringContent(String::from(r#"{"test0": "val0"}"#)));
+//     let mut builder = config_str!(r#"{"test0": "val0"}"#);
 //     builder.define_bundle("qa", config_sources); //just store
 //     builder.define_bundle("prod", config_sources); //just store
 //     buider.merge_bundle(env!("JSON_CONFIG_ENV")); //clone and lazy merge env
-//     builder.to_out_file("json_config.json");
+//     builder.to_compiled("json_config.json");
 // }
 
 // fn main(){
-//     let builder = from_out_file!("json_config.json");
+//     let builder = config_compiled!("json_config.json");
 //     println!("{}", builder.to_string_pretty());
 // }
