f0aa2404dd4c077f12de99c3775dba2f29ed69b2
Hemal Harindaka <hathulathmudali@gmail.com>
2017-11-26 15:58:53 +0530

    alternative error handling approach

--- src/error.rs
+++ src/error.rs
@@ -5,18 +5,54 @@ use std::fmt;
 use std::io;
 use std::env;
 
-#[derive(Debug)]
-pub enum JsonConfigError{
-    ConfigDefinition(String),
+//#[derive(Debug)]
+pub enum ErrorKind{
+    ConfigDefinition,
     //BundleNotFound(String),
-    EnvVar(env::VarError),
-    Io(io::Error),
-    SerdeJson(serde_json::error::Error)    
+    EnvVar, //(env::VarError),
+    Io, //(io::Error),
+    SerdeJson //(serde_json::error::Error)    
 }
 
-impl From<String> for JsonConfigError {
-    fn from(err: String) -> JsonConfigError {
-        JsonConfigError::ConfigDefinition(err)
+pub struct ConfigDefinitionError {
+    err: String
+}
+
+impl fmt::Display for ConfigDefinitionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid configuration definition. {}", self.err)
+    }
+}
+
+impl error::Error for ConfigDefinitionError {
+    fn description(&self) -> &str {
+        format!("Invalid configuration definition. {}", self.err)
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
+
+
+
+pub struct JsonConfigError {
+    kind: ErrorKind,
+    err: error::Error
+}
+
+impl JsonConfigError {
+    pub fn new(kind: ErrorKind, err: error::Error) -> JsonConfigError {
+        JsonConfigError {
+            kind: kind,
+            err: err
+        }
+    }
+}
+
+impl From<ConfigDefinitionError> for JsonConfigError {
+    fn from(err: ConfigDefinitionError) -> JsonConfigError {
+        JsonConfigError::new(ErrorKind::ConfigDefinition, err)
     }
 }
 
@@ -28,54 +64,54 @@ impl From<String> for JsonConfigError {
 
 impl From<env::VarError> for JsonConfigError {
     fn from(err: env::VarError) -> JsonConfigError {
-        JsonConfigError::EnvVar(err)
+        JsonConfigError::new(ErrorKind::EnvVar, err)
     }
 }
 
 impl From<io::Error> for JsonConfigError {
     fn from(err: io::Error) -> JsonConfigError {
-        JsonConfigError::Io(err)
+        JsonConfigError::new(ErrorKind::Io, err)
     }
 }
 
 impl From<serde_json::error::Error> for JsonConfigError {
     fn from(err: serde_json::error::Error) -> JsonConfigError {
-        JsonConfigError::SerdeJson(err)
+        JsonConfigError::new(ErrorKind::SerdeJson, err)
     }
 }
 
 impl fmt::Display for JsonConfigError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
+        match *self.kind {
             // Both underlying errors already impl `Display`, so we defer to
             // their implementations.
-            JsonConfigError::ConfigDefinition(ref err) => write!(f, "Invalid configuration definition encountered. {}", err),
+            ErrorKind::ConfigDefinition => write!(f, "Invalid configuration definition encountered. {}", self.err),
             //JsonConfigError::BundleNotFound(ref err) => write!(f, "The bundle {} does not exist.", err),
-            JsonConfigError::EnvVar(ref err) => write!(f, "Encountered std::env::VarError: {}", err),
-            JsonConfigError::Io(ref err) => write!(f, "Encountered std::io::Error: {}", err),
-            JsonConfigError::SerdeJson(ref err) => write!(f, "Encountered serde_json::error::Error: {}", err),
+            ErrorKind::EnvVar => write!(f, "Encountered std::env::VarError: {}", self.err),
+            ErrorKind::Io => write!(f, "Encountered std::io::Error: {}", self.err),
+            ErrorKind::SerdeJson => write!(f, "Encountered serde_json::error::Error: {}", self.err),
         }
     }
 }
 
 impl error::Error for JsonConfigError {
     fn description(&self) -> &str {
-        match *self {
-            JsonConfigError::ConfigDefinition(ref err) => format!("Invalid configuration definition encountered. {}", err).to_string().as_str(),
+        match *self.kind {
+            ErrorKind::ConfigDefinition => error::Error::description(self.err),
             //JsonConfigError::BundleNotFound(ref err) => format!("The bundle {} does not exist.", err),
-            JsonConfigError::EnvVar(ref err) => error::Error::description(err),
-            JsonConfigError::Io(ref err) => error::Error::description(err),
-            JsonConfigError::SerdeJson(ref err) => error::Error::description(err),
+            ErrorKind::EnvVar => error::Error::description(self.err),
+            ErrorKind::Io => error::Error::description(self.err),
+            ErrorKind::SerdeJson => error::Error::description(self.err),
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            JsonConfigError::ConfigDefinition(ref err) => None,
+        match *self.kind {
+            ErrorKind::ConfigDefinition => Some(self.err),
             //JsonConfigError::BundleNotFound(ref err) => None,
-            JsonConfigError::Io(ref err) => Some(err),
-            JsonConfigError::SerdeJson(ref err) => Some(err),
-            JsonConfigError::EnvVar(ref err) => Some(err),
+            ErrorKind::Io => Some(self.err),
+            ErrorKind::SerdeJson => Some(self.err),
+            ErrorKind::EnvVar => Some(self.err),
         }
     }
 }
